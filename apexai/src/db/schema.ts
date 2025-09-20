d1bd25950c3c0e77a5287ee923a5e8feda1e9616
Shivam Bisht <rajshivam2745@gmail.com>
2025-09-20 23:48:38 +0530

    schema added for session

--- apexai/src/db/schema.ts
+++ apexai/src/db/schema.ts
@@ -1,6 +1,25 @@
-import { pgTable, integer, varchar, uuid } from "drizzle-orm/pg-core"
+import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core"
+import { user } from "../../auth-schema"
 
 export const userTable = pgTable("user", {
-    id: uuid(),
-}) 
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    emailverified: boolean("email_verified").$defaultFn(() => false).notNull(),
+    image: text("image"),
+    createdAt: timestamp("created_at").$defaultFn(() => new Date()).notNull(),
+    updatedAt: timestamp("updated_at").$defaultFn(() => new Date()).notNull(),
+})
 
+
+export const sessionTable = pgTable("session", {
+    id: text("id").primaryKey(),
+    expiresAt: timestamp("expires_at").notNull(),
+    token: text("token").notNull().unique(),
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").notNull(),
+    ipAddress: text("ip_address"),
+    userAgent: text("user_agent"),
+    userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" })
+
+})
