ff1697fb21ef35e1de0527cc4a6cfc84a68b4cd9
Shivam Bisht <rajshivam2745@gmail.com>
2025-09-22 23:26:21 +0530

    account and verification table added

--- apexai/src/db/schema.ts
+++ apexai/src/db/schema.ts
@@ -1,7 +1,6 @@
 import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core"
-import { user } from "../../auth-schema"
 
-export const userTable = pgTable("user", {
+export const user = pgTable("user", {
     id: text("id").primaryKey(),
     name: text("name").notNull(),
     email: text("email").notNull().unique(),
@@ -12,7 +11,7 @@ export const userTable = pgTable("user", {
 })
 
 
-export const sessionTable = pgTable("session", {
+export const session = pgTable("session", {
     id: text("id").primaryKey(),
     expiresAt: timestamp("expires_at").notNull(),
     token: text("token").notNull().unique(),
@@ -23,3 +22,35 @@ export const sessionTable = pgTable("session", {
     userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" })
 
 })
+
+export const account = pgTable("account", {
+    id: text("id").primaryKey(),
+    accountId: text("account_id").notNull(),
+    providerId: text("provider_id").notNull(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => user.id, { onDelete: "cascade" }),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    idToken: text("id_token"),
+    accessTokenExpiresAt: timestamp("access_token_expires_at"),
+    refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+    scope: text("scope"),
+    password: text("password"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+        .$onUpdate(() => /* @__PURE__ */ new Date())
+        .notNull(),
+});
+
+export const verification = pgTable("verification", {
+    id: text("id").primaryKey(),
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+        .defaultNow()
+        .$onUpdate(() => /* @__PURE__ */ new Date())
+        .notNull(),
+});
