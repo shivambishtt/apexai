2a26ec41f7d0d2d3053b6808cece2ab91de6d3a9
Shivam Bisht <rajshivam2745@gmail.com>
2025-09-24 23:48:37 +0530

    page.tsx modified

--- apexai/src/app/page.tsx
+++ apexai/src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 function page() {
+  const [name, setName] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
   return (
-    <Button>
-      Test
-    </Button>
+    <div>
+      <Input placeholder="name" value={name} onChange={((event) => setName(event.target.value))} />
+      <Input placeholder=" email" value={email} onChange={((event) => setemail(event.target.value))} />
+      <Input placeholder=" password" type="password" value={password} onChange={((event) => setpassword(event.target.value))} />
+      <Button>
+        Create User
+      </Button>
+    </div>
   )
 }
 
