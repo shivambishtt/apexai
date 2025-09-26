63c15b892abad62329b8e97dbe4b5c1e34a684e6
Shivam Bisht <rajshivam2745@gmail.com>
2025-09-26 23:27:41 +0530

    auth and page component changes

--- apexai/src/app/page.tsx
+++ apexai/src/app/page.tsx
@@ -1,23 +1,54 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useState } from "react"
+import { authClient } from "@/lib/auth-client"
 
-function page() {
-  const [name, setName] = useState("")
-  const [email, setemail] = useState("")
-  const [password, setpassword] = useState("")
+function Page() {
+  const [name, setName] = useState<string>("")
+  const [email, setemail] = useState<string>("")
+  const [password, setpassword] = useState<string>("")
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      name,
+      email,
+      password
+    }, {
+      onError: () => {
+        window.alert("Something went wrong");
+      },
+      onSuccess: () => {
+        window.alert("Success ")
+      }
+    }
+    )
+  }
+
   return (
-    <div>
-      <Input placeholder="name" value={name} onChange={((event) => setName(event.target.value))} />
-      <Input placeholder=" email" value={email} onChange={((event) => setemail(event.target.value))} />
-      <Input placeholder=" password" type="password" value={password} onChange={((event) => setpassword(event.target.value))} />
-      <Button>
-        Create User
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={((event) => setName(event.target.value))} />
+
+      <Input
+        placeholder=" email"
+        value={email}
+        onChange={((event) => setemail(event.target.value))} />
+
+      <Input
+        placeholder=" password"
+        type="password"
+        value={password}
+        onChange={((event) => setpassword(event.target.value))} />
+
+      <Button onClick={onSubmit}>
+        Signup
       </Button>
     </div>
   )
 }
 
-export default page
+export default Page
