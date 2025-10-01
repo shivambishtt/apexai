fbac7425ff919ecae1f26b18b13b6b2f96139c9b
unknown <rajshivam2745@gmail.com>
2025-10-01 23:56:46 +0530

    views added

--- apexai/src/modules/auth/ui/views/signin-view.tsx
+++ apexai/src/modules/auth/ui/views/signin-view.tsx
@@ -1,11 +1,17 @@
-import { Card } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
 
 function SigninView() {
   return (
-    <Card>
-        
+   <div className='flex flex-col gap-6'>
+    <Card className='overflow-hidden p-0'>
+
+    <CardContent className='grid p-0 md:grid-cols-2'>
+    <p>Col 1</p>
+    <p>Col 2</p>
+    </CardContent>
     </Card>
+   </div>
   )
 }
 
